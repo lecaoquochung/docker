2dcc74940c163b891cb4e1eee1de46d3eb3d830b
HL <lecaoquochung@gmail.com>
2016-09-23 09:26:03 +0900

    db init schema

--- centos6-php/db/init.d/schema.sql
+++ centos6-php/db/init.d/schema.sql
@@ -8,3 +8,7 @@ INSERT INTO posts (title, text) VALUES ('Blog #2', 'Welcome to my second blog po
 INSERT INTO posts (title, text) VALUES ('Blog #3', 'Welcome to my third blog post');
 INSERT INTO posts (title, text) VALUES ('Blog #4', 'Welcome to my fourth blog post');
 INSERT INTO posts (title, text) VALUES ('Blog #5', 'Welcome to my fifth blog post');
+
+CREATE TABLE albums (id INTEGER PRIMARY KEY AUTO_INCREMENT, artist varchar(100) NOT NULL, title varchar(100) NOT NULL);
+INSERT INTO albums (artist, title) VALUES ('The Military Wives', 'In My Dreams');
+INSERT INTO albums (artist, title) VALUES ('Adele', '21');
